e5d497ff5924ccf02f816f6258799ae435593464
Marco Richetta <marcorichetta@gmail.com>
2019-07-21 22:13:28 -0300

    uibuild

---
+++ build/precache-manifest.4a283b7d54a18683460eae4e6f944a56.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9e98c1b2f279a719d4de41b3e8b814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "99791faf5d17e4d9b253",
+    "url": "/static/js/main.dc1bb8b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(bodyParser.json())
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 let persons = [
     {
         "name": "Arto Hellas",
